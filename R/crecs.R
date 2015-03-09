219ff79ac19bc4ac591feb933c4487951a5a17b6
janhoo <holstein@uni-bremen.de>
2015-03-09 16:11:47 +0100

    initial commit

---
+++ R/crecs.R
@@ -0,0 +1,15 @@
+#' crecs: Comfy Multivariate Regression and Cross-validation with Spatial Objects
+#' 
+#' crecs provides wrapper functions for some popular multivariate regression and catgorization / species distribution modelling methods.
+#' 
+#' It has three main goals:
+#' \itemize{
+#' \item Free you from tedious tasks trough easy automation.
+#' \item Provide a large variety of model performance metrics.
+#' \item Provide model validation techniques like cross-validation, null-models, 
+#' and  exploration of spatial autocorrelation.
+#' }
+#' @docType package
+#' @name crecs
+#' @useDynLib crecs
+NULL
