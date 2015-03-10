dc6a55f713956db8a551bd6778cf6f48591e395d
janhoo <holstein@uni-bremen.de>
2015-03-10 15:57:24 +0100

    some example data

---
+++ R/deutschebucht.R
@@ -0,0 +1,12 @@
+#' @name deutschebucht
+#' @title Evironmental conditions in the Deutsche Bucht
+#' @description his data set gives the depth, median grain size, sorting coefficient, 
+#' mud content, average shear stress by currents, average shear stress by waves, 
+#' winter bottom water temperature, and average primary production of 
+#' the south-east sector of the North Sea, the Deutsche Bucht.
+#' @docType data
+#' @usage data(deutschebucht)
+#' @format a \code{RasterBrick} containing 8 layers.
+#' @source various
+#' @author Jan M Holstein , 2015-03-05
+NULL
---
+++ R/species.R
@@ -0,0 +1,10 @@
+#' @name species
+#' @title Virtual sampling of three virtual species from the Deutsche Bucht
+#' @description his data set gives 300 random sampling locations within the Deutsche Bucht with 
+#' 3 virtual species including sampling error.
+#' @docType data
+#' @usage data(species)
+#' @format a \code{SpatialPointsDataFrame} containing 3x300 observations.
+#' @source made up with virtualspecies package
+#' @author Jan M Holstein , 2015-03-05
+NULL
