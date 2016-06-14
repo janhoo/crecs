12488c4920d5a2b644505725bcee00b91c7910fa
janhoo <jm.smith@gmx.net>
2016-06-14 15:33:52 +0200

    x

--- R/crecs.R
+++ R/crecs.R
@@ -6,7 +6,7 @@
 #' \describe{
 #' \itemize{
 #' \item Free you from tedious tasks trough easy automation.
-#' \item Provide a large variety of model performance metrics.
+#' \item Provide a large variety of model performance metrics..
 #' \item Provide model validation techniques like cross-validation, null-models, 
 #' }
 #' and  exploration of spatial autocorrelation.
